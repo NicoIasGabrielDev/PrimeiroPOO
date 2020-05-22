8e4a0f223f331c62c4692a8dbb5ac20d5e969bc4
nicolasgabrieloficial <61585068+nicolasgabrieloficial@users.noreply.github.com>
2020-05-22 11:34:57 -0300

    Add files via upload

---
+++ PrimeiroPOO/Personagem.cs
@@ -0,0 +1,18 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        
+        public string nome = "Neymar";
+
+        public int idade = 28;
+
+        public string time;
+
+        public string Atacar()
+        {
+            return "Ele fez o gol!";
+        }
+
+    }
+}
---
+++ PrimeiroPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem neymar = new Personagem();
+            neymar.time = "PSG";
+
+           Console.WriteLine(neymar.nome);
+           Console.WriteLine(neymar.time);
+           Console.WriteLine(neymar.Atacar());
+          
+        }
+    }
+}
